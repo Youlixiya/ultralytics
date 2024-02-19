b3ac4b9315f728857bba6dc877aeab7f42c5172b
yl <2317676993@qq.com>
2024-02-19 15:18:53 +0800

    yolo-world-sam

--- ultralytics/models/yolo/model.py
+++ ultralytics/models/yolo/model.py
@@ -1,11 +1,14 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
-
+import numpy as np
+from PIL.Image import Image
 from pathlib import Path
-
+import torch
 from ultralytics.engine.model import Model
 from ultralytics.models import yolo
 from ultralytics.nn.tasks import ClassificationModel, DetectionModel, OBBModel, PoseModel, SegmentationModel, WorldModel
+from ultralytics.nn.modules import C2f
 from ultralytics.utils import yaml_load, ROOT
+from ultralytics.models.edge_sam.utils.transforms import ResizeLongestSide
 
 
 class YOLO(Model):
@@ -103,3 +106,68 @@ class YOLOWorld(Model):
         # self.predictor = None  # reset predictor otherwise old names remain
         if self.predictor:
             self.predictor.model.names = classes
+            
+class YOLOWorldSAM(Model):
+    """YOLO-World object detection model."""
+    pixel_mean = [123.675, 116.28, 103.53]
+    pixel_std = [58.395, 57.12, 57.375]
+    img_size = 1024
+    def __init__(self, model="yolov8s-world-sam.pt") -> None:
+        """
+        Initializes the YOLOv8-World model with the given pre-trained model file. Supports *.pt and *.yaml formats.
+
+        Args:
+            model (str): Path to the pre-trained model. Defaults to 'yolov8s-world.pt'.
+        """
+        super().__init__(model=model, task="detect")
+
+        # Assign default COCO class names
+        self.model.names = yaml_load(ROOT / "cfg/datasets/coco8.yaml").get("names")
+        self.transform = ResizeLongestSide(self.img_size)
+
+    @property
+    def task_map(self):
+        """Map head to model, validator, and predictor classes."""
+        return {
+            "detect": {
+                "model": WorldModel,
+                "validator": yolo.detect.DetectionValidator,
+                "predictor": yolo.detect.DetectionPredictor,
+            }
+        }
+
+    def set_classes(self, classes):
+        """
+        Set classes.
+
+        Args:
+            classes (List(str)): A list of categories i.e ["person"].
+        """
+        self.model.set_classes(classes)
+        # Remove background if it's given
+        background = " "
+        if background in classes:
+            classes.remove(background)
+        self.model.names = classes
+
+        # Reset method class names
+        # self.predictor = None  # reset predictor otherwise old names remain
+        if self.predictor:
+            self.predictor.model.names = classes
+    
+    def preprocess(self, x):
+        """Normalize pixel values and pad to a square input."""
+        if isinstance(x, Image):
+            x = np.array(Image)
+        x = self.transform.apply_image(x)
+        x = torch.as_tensor(x, device=self.device)
+        x = x.permute(2, 0, 1).contiguous()[None, :, :, :]
+        # Normalize colors
+        x = (x - self.pixel_mean) / self.pixel_std
+
+        # Pad
+        h, w = x.shape[-2:]
+        padh = self.img_size - h
+        padw = self.img_size - w
+        x = torch.nn.functional.pad(x, (0, padw, 0, padh))
+        return x
